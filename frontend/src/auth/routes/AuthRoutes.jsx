70a904d6c3a99ad50758411687133f8fceff6f16
Vaibhav Rohaan <82140153+vaibhxv@users.noreply.github.com>
2024-07-09 12:53:59 +0530

    Update AuthRoutes.jsx

--- frontend/src/auth/routes/AuthRoutes.jsx
+++ frontend/src/auth/routes/AuthRoutes.jsx
@@ -1,10 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import {
-  ConfirmAccountPage,
-  ForgotPasswordPage,
   LoginPage,
-  RegisterPage,
-  ResetPasswordPage
 } from '../pages';
 
 const AuthRoutes = [
@@ -12,22 +8,7 @@ const AuthRoutes = [
     path: 'login',
     element: <LoginPage />
   },
-  {
-    path: 'register',
-    element: <RegisterPage />
-  },
-  {
-    path: 'forgot-password',
-    element: <ForgotPasswordPage />
-  },
-  {
-    path: 'reset-password/:token',
-    element: <ResetPasswordPage />
-  },
-  {
-    path: 'confirm-account/:token',
-    element: <ConfirmAccountPage />
-  },
+ 
   {
     index: true,
     element: <Navigate to='/auth/login' />
